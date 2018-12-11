acf2820254e44d77822f4d654b44ddfe8a45a6f0
Carlos Andres Robles <34143034+carlosrh18@users.noreply.github.com>
2018-12-11 10:28:35 -0600

    Add files via upload

---
+++ go/basic_server.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gorilla/mux"
+	"github.com/go-redis/redis"
+	"html/template"
+)
+
+var client *redis.Client
+var templates *template.Template
+
+
+func main() {
+	client = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",  //redis port DB
+	})
+	templates = template.Must(template.ParseGlob("templates/*.html"))
+	r := mux.NewRouter()
+	r.HandleFunc("/index", indexGetHandler).Methods("GET")
+	r.HandleFunc("/index", indexPostHandler).Methods("POST")
+	fs := http.FileServer(http.Dir("./static/"))
+	r.PathPrefix("/static/")
+	http.Handle("/", r)
+	http.ListenAndServe(":8080", nil)
+}
+
+
+func indexGetHandler(w http.ResponseWriter, r *http.Request) {
+	comments, err := client.LRange("comments", 0, 10).Result()
+	if err != nil {
+		return
+	}
+	templates.ExecuteTemplate(w, "index.html", comments)
+}
+
+func indexPostHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	comment := r.PostForm.Get("comment")
+	client.LPush("comments", comment)
+	http.Redirect(w, r, r.Header.Get("Referer"), 302)
+}
+
+
+
+
